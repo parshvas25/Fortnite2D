bd49e66700fced60f86d7dd6edb4dc9e3c9d66e2
Gautam Gireesh <gautamgireesh@gmail.com>
2021-04-10 03:40:10 +0000

    push ref

--- setup.bash
+++ setup.bash
@@ -1,3 +1,8 @@
+'''
+    PLEASE NOTE: In /src/controller/controller.js, please change the IP address of the global variable called url and our socket variable inside initsocket()
+    to your respective VM-IP.
+'''
+
 rm -rf node_modules
 npm install
 
